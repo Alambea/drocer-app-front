09517760c4dcedc6e0989f25bf37ae8378fac589
Ana Lambea <analambeap@gmail.com>
2023-09-05 12:09:32 +0200

    Create Navigation component structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import "./Header.scss";
 
-const Header = () => {
+const Header = (): React.ReactElement => {
   return (
     <header className="header">
       <a href="/home">
