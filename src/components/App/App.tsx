c46410312b82504643a6e52b80bdbfcabea60c6b
Ana Lambea <analambeap@gmail.com>
2023-09-06 00:41:09 +0200

    Add Router and routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,14 +4,19 @@ import Header from "../Header/Header";
 import Navigation from "../Navigation/Navigation";
 import "./App.scss";
 import { auth } from "../../firebase";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const App = (): React.ReactElement => {
   const [user] = useAuthState(auth);
+
   return (
     <div className="container">
       <Header />
       <main className="main-container">
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
         {user && <Navigation />}
       </main>
     </div>
