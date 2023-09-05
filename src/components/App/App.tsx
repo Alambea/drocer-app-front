5485e7175953242f9bb6c0165cc3facd6c2e14e5
Ana Lambea <analambeap@gmail.com>
2023-09-05 12:11:29 +0200

    Set Navigation component styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
 import "./App.scss";
 
 const App = (): React.ReactElement => {
   return (
     <div className="container">
-      <Header></Header>
+      <Header />
+      <Navigation />
     </div>
   );
 };
