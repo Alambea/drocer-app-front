c13f9a2fd2537a6453a77195b969aa070622f7f6
Ana Lambea <analambeap@gmail.com>
2023-09-06 00:15:19 +0200

    Test navigation when user is logged

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { User } from "firebase/auth/cordova";
+import auth, { AuthStateHook } from "react-firebase-hooks/auth";
 
 describe("Given an App component", () => {
   describe("When it is rendered", () => {
@@ -32,6 +34,35 @@ describe("Given an App component", () => {
       const image = screen.getByAltText(expectedAltText);
 
       expect(image).toBeInTheDocument();
+    });
+
+    describe("And the user 'Ana' is logged", () => {
+      test("Then it should show two links with the text 'Add' and 'Records'", () => {
+        const expectedAddText = /add/i;
+        const expectedRecordsText = /records/i;
+        const user: Partial<User> = { displayName: "Ana" };
+
+        const authStateHookMock: Partial<AuthStateHook> = [
+          user as User,
+          false,
+          new Error(""),
+        ];
+
+        auth.useAuthState = vi.fn().mockReturnValue(authStateHookMock);
+        render(
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>,
+        );
+
+        const addLink = screen.getByRole("link", { name: expectedAddText });
+        const recordsLink = screen.getByRole("link", {
+          name: expectedRecordsText,
+        });
+
+        expect(addLink).toBeInTheDocument();
+        expect(recordsLink).toBeInTheDocument();
+      });
     });
   });
 });
