054ad505dc8d7213d4414dc9041f4d004e8f1abd
Ana Lambea <analambeap@gmail.com>
2023-09-07 00:03:30 +0200

    Add loadRecords action

--- src/store/records/recordsSlice.ts
+++ src/store/records/recordsSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { RecordsState } from "./types";
+import { Record } from "../../types";
 
 const initialRecordsState: RecordsState = {
   records: [],
@@ -8,7 +9,16 @@ const initialRecordsState: RecordsState = {
 export const recordsSlice = createSlice({
   name: "records",
   initialState: initialRecordsState,
-  reducers: {},
+  reducers: {
+    loadRecords: (
+      currentRecordState,
+      action: PayloadAction<Record[]>,
+    ): RecordsState => ({
+      ...currentRecordState,
+      records: action.payload,
+    }),
+  },
 });
 
 export const recordsReducer = recordsSlice.reducer;
+export const { loadRecords: loadRecords } = recordsSlice.actions;
