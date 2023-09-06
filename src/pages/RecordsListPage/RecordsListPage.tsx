2703c838328b452e4990afe96f9ce7b127bc0a17
Ana Lambea <analambeap@gmail.com>
2023-09-06 10:22:37 +0200

    Add RecordsListPage page structure

---
+++ src/pages/RecordsListPage/RecordsListPage.tsx
@@ -0,0 +1,9 @@
+const RecordsListPage = (): React.ReactElement => {
+  return (
+    <section className="records-container">
+      <h2 className="records-title">Records</h2>
+    </section>
+  );
+};
+
+export default RecordsListPage;
