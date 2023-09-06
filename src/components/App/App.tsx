b4fc2cfe7dd80a99851a2cf63e51b3115b308038
Ana Lambea <analambeap@gmail.com>
2023-09-06 23:27:33 +0200

    Add ProtectedRoute component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,29 @@ import HomePage from "../../pages/HomePage/HomePage";
 import { Navigate, Route, Routes } from "react-router-dom";
 import RecordsListPage from "../../pages/RecordsListPage/RecordsListPage";
 import Layout from "../Layout/Layout";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 
 const App = (): React.ReactElement => {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Navigate to="/home" />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/records" element={<RecordsListPage />} />
+        <Route
+          path="/records"
+          element={
+            <ProtectedRoute>
+              <RecordsListPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Layout>
   );
