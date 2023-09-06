d1242d26bf4bfef35de3efc99df5c72086fdca03
Ana Lambea <analambeap@gmail.com>
2023-09-06 20:19:54 +0200

    Remove Link element from header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { paths } from "../../routers/paths";
 import "./Header.scss";
 
 const Header = (): React.ReactElement => {
   return (
     <header className="header">
-      <Link to={paths.home}>
-        <h1 className="header__title">Drocer</h1>
-      </Link>
-      <Link to={paths.home}>
-        <img
-          src="./images/drocer_logo.svg"
-          alt="Drocer's app logo"
-          className="header__logo"
-          width="37"
-          height="26"
-        />
-      </Link>
+      <h1 className="header__title">Drocer</h1>
+      <img
+        src="./images/drocer_logo.svg"
+        alt="Drocer's app logo"
+        className="header__logo"
+        width="37"
+        height="26"
+      />
     </header>
   );
 };
