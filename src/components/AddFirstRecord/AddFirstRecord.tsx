ca29dd37ca4209ef3805bb91f451ade747b29d65
Ana Lambea <analambeap@gmail.com>
2023-09-07 14:12:13 +0200

    Add AddFirstRecord component structure

---
+++ src/components/AddFirstRecord/AddFirstRecord.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AddFirstRecord = (): React.ReactElement => {
+  return (
+    <>
+      <article className="add-first-record">
+        <img
+          src="./images/drocer_logo.svg"
+          alt="Drocer's app logo"
+          className="add-first-record__logo"
+          width="37"
+          height="26"
+        />
+        <h2 className="add-first-record__title">Add your first record</h2>
+      </article>
+    </>
+  );
+};
+
+export default AddFirstRecord;
--- src/pages/RecordsListPage/RecordsListPage.tsx
+++ src/pages/RecordsListPage/RecordsListPage.tsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import "./RecordsListPage.scss";
-import { useAppDispatch } from "../../store";
+import { useAppDispatch, useAppSelector } from "../../store";
 import { recordsData } from "../../data/recordData";
 import { loadRecordsActionCreator } from "../../store/records/recordsSlice";
 import RecordsList from "../../components/RecordList/RecordsList";
+import AddFirstRecord from "../../components/AddFirstRecord/AddFirstRecord";
 
 const RecordsListPage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
+  const records = useAppSelector((state) => state.recordsState.records);
+
+  const hasRecords = records.length < 0;
 
   useEffect(() => {
     const records = recordsData;
@@ -16,8 +20,14 @@ const RecordsListPage = (): React.ReactElement => {
 
   return (
     <section className="records-page">
-      <h2 className="records-page__title">Records</h2>
-      <RecordsList />
+      {hasRecords ? (
+        <>
+          <h2 className="records-page__title">Records</h2>
+          <RecordsList />
+        </>
+      ) : (
+        <AddFirstRecord />
+      )}
     </section>
   );
 };
