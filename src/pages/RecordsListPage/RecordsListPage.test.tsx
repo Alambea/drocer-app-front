7c780edb9f2fd554a1ec266c7b852ec51bec4319
Ana Lambea <analambeap@gmail.com>
2023-09-08 12:58:18 +0200

    Test addFirstRecord from RecordsListPage

--- src/pages/RecordsListPage/RecordsListPage.test.tsx
+++ src/pages/RecordsListPage/RecordsListPage.test.tsx
@@ -5,7 +5,9 @@ import { recordsMock } from "../../mocks/recordsMock";
 import { Provider } from "react-redux";
 
 describe("Given a RecordsListPage page", () => {
-  describe("When it is rendered", () => {
+  vi.mock("react", () => ({ useEffect: vi.fn() }));
+
+  describe("When it is rendered and has records", () => {
     test("Then it should show a heading 'Records'", () => {
       const expectedTitle = "Records";
       const store = setupStore({ recordsState: { records: recordsMock } });
@@ -17,6 +19,23 @@ describe("Given a RecordsListPage page", () => {
       );
 
       const heading = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and doesn't have records", () => {
+    test("Then it should show a heading 'Add your first record'", () => {
+      const expectedHeading = "Add your first record";
+      const store = setupStore({ recordsState: { records: [] } });
+
+      render(
+        <Provider store={store}>
+          <RecordsListPage />
+        </Provider>,
+      );
+
+      const heading = screen.getByRole("heading", { name: expectedHeading });
 
       expect(heading).toBeInTheDocument();
     });
--- src/pages/RecordsListPage/RecordsListPage.tsx
+++ src/pages/RecordsListPage/RecordsListPage.tsx
@@ -14,7 +14,6 @@ const RecordsListPage = (): React.ReactElement => {
 
   useEffect(() => {
     const records = recordsData;
-
     dispatch(loadRecordsActionCreator(records));
   });
 
