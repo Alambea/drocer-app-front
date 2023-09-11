763844b6fcf245f669fc9457242ebb2544e69a1e
Ana Lambea <analambeap@gmail.com>
2023-09-11 21:31:41 +0200

    Add useRecordsApi and getRecords custom hook

---
+++ src/hooks/useRecordsApi.ts
@@ -0,0 +1,54 @@
+import { useIdToken } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import axios from "axios";
+import { Record, RecordApi } from "../types";
+
+const useRecordsApi = () => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const [user] = useIdToken(auth);
+
+  const getRecords = async () => {
+    const token = await user?.getIdToken();
+
+    try {
+      const { data: apiRecords } = await axios.get<{ records: RecordApi[] }>(
+        `${apiUrl}/records`,
+        { headers: { Authorization: `Bearer ${token}` } },
+      );
+
+      const records = apiRecords.records.map<Record>(
+        ({
+          _id,
+          artist,
+          cover,
+          description,
+          genres,
+          label,
+          length,
+          rating,
+          record,
+          releaseDate,
+        }) => ({
+          id: _id,
+          artist,
+          cover,
+          description,
+          genres,
+          label,
+          length,
+          rating,
+          record,
+          releaseDate,
+        }),
+      );
+
+      return records;
+    } catch {
+      throw new Error("Couldn't load records");
+    }
+  };
+
+  return { getRecords };
+};
+
+export default useRecordsApi;
