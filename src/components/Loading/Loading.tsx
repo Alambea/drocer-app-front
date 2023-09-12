bec97d1c4478398c6c95f97263f6ac31cffe03a0
Ana Lambea <analambeap@gmail.com>
2023-09-12 12:11:54 +0200

    Create loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = (): React.ReactElement => {
+  return (
+    <div className="loading" aria-label="loading">
+      <span className="loader" />
+    </div>
+  );
+};
+
+export default Loading;
