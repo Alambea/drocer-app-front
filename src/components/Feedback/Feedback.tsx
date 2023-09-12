eed6424b8aca4740fc7226d2561186a6a27a6dcd
Ana Lambea <analambeap@gmail.com>
2023-09-12 23:42:31 +0200

    Add Feedback toast component styles

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -1,5 +1,6 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./Feedback.scss";
 
 const Feedback = () => {
   return <ToastContainer />;
