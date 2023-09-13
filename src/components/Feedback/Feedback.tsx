4071a8dc86070015ebe7dd2facca1c3f28c006bf
Ana Lambea <analambeap@gmail.com>
2023-09-13 16:20:16 +0200

    Move Feedback scss to Layout and delete component

--- src/components/Feedback/Feedback.tsx
+++
@@ -1,9 +0,0 @@
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "./Feedback.scss";
-
-const Feedback = () => {
-  return <ToastContainer />;
-};
-
-export default Feedback;
