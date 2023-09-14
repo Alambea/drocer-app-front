753ac533ff3a458e7a7a3a6a68cd0750a9062a23
Ana Lambea <analambeap@gmail.com>
2023-09-14 12:11:32 +0200

    Add AddRecordPage page structure

---
+++ src/pages/AddRecordPage/AddRecordPage.tsx
@@ -0,0 +1,9 @@
+const AddRecordPage = (): React.ReactElement => {
+  return (
+    <article className="add-record">
+      <h1 className="add-record__title">Add a new record</h1>
+    </article>
+  );
+};
+
+export default AddRecordPage;
