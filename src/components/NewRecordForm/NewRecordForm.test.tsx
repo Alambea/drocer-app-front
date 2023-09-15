85d70868ff2877730c6e7114c78f3caf11c30f2a
Ana Lambea <analambeap@gmail.com>
2023-09-15 06:29:58 +0200

    Test NewRecordForm form state

--- src/components/NewRecordForm/NewRecordForm.test.tsx
+++ src/components/NewRecordForm/NewRecordForm.test.tsx
@@ -1,20 +1,32 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import NewRecordForm from "./NewRecordForm";
 import AddRecordPage from "../../pages/AddRecordPage/AddRecordPage";
+import { recordMock } from "../../mocks/recordsMock";
 
 describe("Given a NewRecordForm component", () => {
+  const artistInputLabel = "Artist";
+  const recordInputLabel = "Record";
+  const releaseDateInputLabel = "Release Date";
+  const ratingInputLabel = "Rating 1/5";
+  const descriptionInputLabel = "Description";
+  const lengthInputLabel = "Length";
+  const labelInputLabel = "Label";
+  const genresInputLabel = "Genres";
+  const coverInputLabel = "Image URL";
+
+  const typedArtist = recordMock.artist;
+  const typedRecord = recordMock.record;
+  const typedReleaseDate = recordMock.releaseDate;
+  const selectedRating = recordMock.rating;
+  const typedDescription = recordMock.description;
+  const typedLength = recordMock.length;
+  const typedLabel = recordMock.label;
+  const typedGenres = recordMock.genres;
+  const typedCover = recordMock.cover;
+
   describe("When it's rendered", () => {
     test("Then it should show an 'Artist', 'Record', 'Release Date', 'Rating 1/5', 'Description', 'Length', 'Label', 'Genres', and a 'Image URL' fields", () => {
-      const artistInputLabel = "Artist";
-      const recordInputLabel = "Record";
-      const releaseDateInputLabel = "Release Date";
-      const ratingInputLabel = "Rating 1/5";
-      const descriptionInputLabel = "Description";
-      const lengthInputLabel = "Length";
-      const labelInputLabel = "Label";
-      const genresInputLabel = "Genres";
-      const imageUrlInputLabel = "Image URL";
-
       render(<NewRecordForm />);
 
       const artistInput = screen.getByLabelText(artistInputLabel);
@@ -25,7 +37,7 @@ describe("Given a NewRecordForm component", () => {
       const lengthInput = screen.getByLabelText(lengthInputLabel);
       const labelInput = screen.getByLabelText(labelInputLabel);
       const genresInput = screen.getByLabelText(genresInputLabel);
-      const imageUrlInput = screen.getByLabelText(imageUrlInputLabel);
+      const coverInput = screen.getByLabelText(coverInputLabel);
 
       expect(artistInput).toBeInTheDocument();
       expect(recordInput).toBeInTheDocument();
@@ -35,7 +47,7 @@ describe("Given a NewRecordForm component", () => {
       expect(lengthInput).toBeInTheDocument();
       expect(labelInput).toBeInTheDocument();
       expect(genresInput).toBeInTheDocument();
-      expect(imageUrlInput).toBeInTheDocument();
+      expect(coverInput).toBeInTheDocument();
     });
 
     test("Then it should a button with the text 'Add'", () => {
@@ -48,6 +60,44 @@ describe("Given a NewRecordForm component", () => {
       });
 
       expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user types 'FKA Twigs', 'LP1', 2014, 4, 'LP1 is the debut studio album by English singer-songwriter FKA Twigs, released on 6 August 2014 by Young Turks. Production for the album is handled by FKA Twigs herself, alongside Emile Haynie, Arca, Cy An, Devonté Hynes, Clams Casino, Paul Epworth, Sampha and Tic.', '40:46', 'Young Turks', 'Avant-pop, electronic, art pop R&B, trip hop' and 'http://example.com/image.png' on each input", () => {
+    test("Then the inputs should have the typed value", async () => {
+      render(<NewRecordForm />);
+
+      const artistInput = screen.getByLabelText(artistInputLabel);
+      const recordInput = screen.getByLabelText(recordInputLabel);
+      const releaseDateInput = screen.getByLabelText(releaseDateInputLabel);
+      const ratingInput = screen.getByLabelText(ratingInputLabel);
+      const descriptionInput = screen.getByLabelText(descriptionInputLabel);
+      const lengthInput = screen.getByLabelText(lengthInputLabel);
+      const labelInput = screen.getByLabelText(labelInputLabel);
+      const genresInput = screen.getByLabelText(genresInputLabel);
+      const coverInput = screen.getByLabelText(coverInputLabel);
+
+      await userEvent.type(artistInput, typedArtist);
+      await userEvent.type(recordInput, typedRecord);
+      await userEvent.type(releaseDateInput, typedReleaseDate.toString());
+      await fireEvent.change(ratingInput, {
+        target: { value: selectedRating },
+      });
+      await userEvent.type(descriptionInput, typedDescription);
+      await userEvent.type(lengthInput, typedLength);
+      await userEvent.type(labelInput, typedLabel);
+      await userEvent.type(genresInput, typedGenres);
+      await userEvent.type(coverInput, typedCover);
+
+      expect(artistInput).toHaveValue(typedArtist);
+      expect(recordInput).toHaveValue(typedRecord);
+      expect(releaseDateInput).toHaveValue(typedReleaseDate);
+      expect(ratingInput).toHaveValue(selectedRating.toString());
+      expect(descriptionInput).toHaveValue(typedDescription);
+      expect(lengthInput).toHaveValue(typedLength);
+      expect(labelInput).toHaveValue(typedLabel);
+      expect(genresInput).toHaveValue(typedGenres);
+      expect(coverInput).toHaveValue(typedCover);
     });
   });
 });
--- src/mocks/recordsMock.ts
+++ src/mocks/recordsMock.ts
@@ -8,7 +8,8 @@ export const recordMock: Record = {
   record: "LP1",
   artist: "FKA Twigs",
   cover: "http://example.com/image.png",
-  description: "August",
+  description:
+    "LP1 is the debut studio album by English singer-songwriter FKA Twigs, released on 6 August 2014 by Young Turks. Production for the album is handled by FKA Twigs herself, alongside Emile Haynie, Arca, Cy An, Devonté Hynes, Clams Casino, Paul Epworth, Sampha and Tic.",
   genres: "Avant-pop, electronic, art pop R&B, trip hop",
   label: "Young Turks",
   length: "40:46",
