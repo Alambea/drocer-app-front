a9684d220e87faf0f533b1040e81ba4ca642dd2f
Ana Lambea <analambeap@gmail.com>
2023-09-15 05:35:16 +0200

    Set form state

--- src/components/NewRecordForm/NewRecordForm.tsx
+++ src/components/NewRecordForm/NewRecordForm.tsx
@@ -1,20 +1,60 @@
+import { useState } from "react";
+import { Record } from "../../types";
 import Button from "../Button/Button";
 import "./NewRecordForm.scss";
 
 const NewRecordForm = (): React.ReactElement => {
+  const initialRecordData: Partial<Record> = {
+    artist: "",
+    record: "",
+    releaseDate: 0,
+    rating: 0,
+    description: "",
+    length: "",
+    label: "",
+    genres: "",
+    cover: "",
+  };
+
+  const [newRecord, setNewRecord] =
+    useState<Partial<Record>>(initialRecordData);
+
+  const addNewRecord = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setNewRecord((newRecord) => ({
+      ...newRecord,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
   return (
     <form className="new-record">
       <div className="new-record__group">
         <label htmlFor="artist" className="new-record__label">
           Artist
         </label>
-        <input type="text" id="artist" className="new-record__input" />
+        <input
+          type="text"
+          id="artist"
+          className="new-record__input"
+          value={newRecord.artist}
+          onChange={addNewRecord}
+        />
       </div>
       <div className="new-record__group">
         <label htmlFor="record" className="new-record__label">
           Record
         </label>
-        <input type="text" id="record" className="new-record__input" />
+        <input
+          type="text"
+          id="record"
+          className="new-record__input"
+          value={newRecord.record}
+          onChange={addNewRecord}
+        />
       </div>
       <div className="new-record__group">
         <label htmlFor="releaseDate" className="new-record__label">
@@ -26,6 +66,8 @@ const NewRecordForm = (): React.ReactElement => {
           className="new-record__input"
           min="1500"
           max="2023"
+          value={newRecord.releaseDate === 0 ? "" : newRecord.releaseDate}
+          onChange={addNewRecord}
         />
       </div>
       <div className="new-record__group">
@@ -39,6 +81,8 @@ const NewRecordForm = (): React.ReactElement => {
           max="5"
           step="1"
           className="new-record__input"
+          value={newRecord.rating}
+          onChange={addNewRecord}
         />
       </div>
       <div className="new-record__group">
@@ -48,31 +92,58 @@ const NewRecordForm = (): React.ReactElement => {
         <textarea
           id="description"
           className="new-record__input new-record__input--textarea"
+          maxLength={400}
+          value={newRecord.description}
+          onChange={addNewRecord}
         ></textarea>
       </div>
       <div className="new-record__group">
         <label htmlFor="length" className="new-record__label">
           Length
         </label>
-        <input type="text" id="length" className="new-record__input" />
+        <input
+          type="text"
+          id="length"
+          className="new-record__input"
+          value={newRecord.length}
+          onChange={addNewRecord}
+        />
       </div>
       <div className="new-record__group">
         <label htmlFor="label" className="new-record__label">
           Label
         </label>
-        <input type="text" id="label" className="new-record__input" />
+        <input
+          type="text"
+          id="label"
+          className="new-record__input"
+          value={newRecord.label}
+          onChange={addNewRecord}
+        />
       </div>
       <div className="new-record__group">
         <label htmlFor="genres" className="new-record__label">
           Genres
         </label>
-        <input type="text" id="genres" className="new-record__input" />
+        <input
+          type="text"
+          id="genres"
+          className="new-record__input"
+          value={newRecord.genres}
+          onChange={addNewRecord}
+        />
       </div>
       <div className="new-record__group">
         <label htmlFor="cover" className="new-record__label">
           Image URL
         </label>
-        <input type="url" id="cover" className="new-record__input" />
+        <input
+          type="url"
+          id="cover"
+          className="new-record__input"
+          value={newRecord.cover}
+          onChange={addNewRecord}
+        />
       </div>
       <Button className="new-record__button" disabled={true}>
         Add
