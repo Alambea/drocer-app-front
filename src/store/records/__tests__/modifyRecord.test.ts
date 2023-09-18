34d7bd2458c8a19538ff61066ba4c9cf2e128af5
Ana Lambea <analambeap@gmail.com>
2023-09-18 19:02:30 +0200

    Test modifyRecord action

---
+++ src/store/records/__tests__/modifyRecord.test.ts
@@ -0,0 +1,23 @@
+import { recordsMock } from "../../../mocks/recordsMock";
+import { Record } from "../../../types";
+import { modifyRecordActionCreator, recordsReducer } from "../recordsSlice";
+import { RecordsState } from "../types";
+
+describe("Given a recordsSlice reducer", () => {
+  describe("When it receives a modifyRecord function with for records and a modified one", () => {
+    test("Then it should return a new state with the rating property from de modified record updated", () => {
+      const currentRecordsState: RecordsState = {
+        records: recordsMock,
+      };
+      const modifiedRecord: Record = { ...recordsMock[0], rating: 3 };
+
+      const modifiedBugAction = modifyRecordActionCreator(modifiedRecord);
+      const newRecordsState = recordsReducer(
+        currentRecordsState,
+        modifiedBugAction,
+      );
+
+      expect(newRecordsState.records[0]).toStrictEqual(modifiedRecord);
+    });
+  });
+});
